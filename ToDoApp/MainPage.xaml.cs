b65539a1ede66c8d70b371d8eee2883774aa77a8
Thomas Crowe <wcrowe@g.clemson.edu>
2023-02-10 00:04:47 -0500

    Add project files.

---
+++ ToDoApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace ToDoApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	//Filler data for lists array
+	public List<string> Data { get; set; }
+	= new List<string> { "List 1", "List 2", "List 3" };
+	
+
+
+
+
+	//Get lists from database
+	//public async Task GetLists()
+	//{
+ //       var lists = await App.Database.GetListsAsync();
+ //       Lists.ItemsSource = lists;
+ //   }
+
+	////Command to add a new list
+	//public Command AddListCommand => new(async () =>
+	//{
+ //       await Navigation.PushAsync(new AddListPage());
+ //   });
+
+	
+}
+
