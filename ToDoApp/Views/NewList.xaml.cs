5a60b686891faaaf0e7d6a60aba7c69c070ddd88
Thomas Crowe <wcrowe@g.clemson.edu>
2023-02-20 12:05:27 -0500

    Starting on NewList

--- ToDoApp/Views/NewList.xaml.cs
+++ ToDoApp/Views/NewList.xaml.cs
@@ -1,9 +1,32 @@
+using ToDoApp.DataServices;
+using ToDoApp.Models;
+
+
 namespace ToDoApp.Views;
 
 public partial class NewList : ContentPage
 {
+    private readonly IRestDataService _restDataService;
     public NewList()
     {
         InitializeComponent();
+    }
+
+    private async void Save_Clicked(object sender, EventArgs e)
+    {
+        var list = new ToDo
+        {
+           
+
+
+           
+        };
+        await _restDataService.AddToDoListAsync(list);
+        //await _restDataService.AddItemAsync(list.Items);
+        await Navigation.PopAsync();
+
+        
+
+        await Navigation.PopAsync();
     }
 }
